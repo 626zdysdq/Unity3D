fed6675ab7d0924f0e8ebf0602ebd9d9d4db7e45
626zdysdq <923024128@qq.com>
2018-06-19 20:37:09 +0800

    Homework10

---
+++ AI/ AI/Assets/Scripts/UserGUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class UserGUI : MonoBehaviour
+{
+    GameSceneController scene;
+    QueryGameStatus state;
+    UserActions action;
+
+    float width, height;
+    float castw(float scale) { return (Screen.width - width) / scale; }
+    float casth(float scale) { return (Screen.height - height) / scale; }
+
+    void Start()
+    {
+        scene = GameSceneController.GetInstance();
+        state = GameSceneController.GetInstance() as QueryGameStatus;
+        action = GameSceneController.GetInstance() as UserActions;
+    }
+
+    void OnGUI()
+    {
+        GUI.skin.button.fontSize = GUI.skin.textArea.fontSize = 20;
+
+        if (state.getMessage() != "")
+        {
+			if (GUI.Button(new Rect(350, 220, 100, 50), state.getMessage())) action.restart();
+        }
+        else
+        {
+            if (GUI.RepeatButton(new Rect(10, 10, 100, 40), "Tip")) GUI.TextArea(new Rect(10, 60, 330, 50), scene.gameRule);
+			if (GUI.Button(new Rect(350, 220 , 100 , 50 ), "Next")) StartCoroutine(action.nextmove());
+        }
+    }
+}
---
+++ AI/README.md
@@ -0,0 +1,7 @@
+# AI
+
+--------------------------
+
+视屏链接
+
+>* http://www.iqiyi.com/w_19ryzqnpmx.html
